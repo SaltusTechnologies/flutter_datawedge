29d2e74c76a1d04325dc802425febb2ce1ce29ef
Braxton <Braxtonshutt@gmail.com>
2025-10-22 14:36:03 -0500

    These changes bring the packages source code back to a 2.1.0 state, with the patch needed for the broadcast receiver

--- android/src/main/kotlin/com/circuskitchens/flutter_datawedge/consts/MyMethods.kt
+++ android/src/main/kotlin/com/circuskitchens/flutter_datawedge/consts/MyMethods.kt
@@ -1,10 +1,10 @@
-package src.main.kotlin.com.circuskitchens.flutter_datawedge.consts
+package com.circuskitchens.flutter_datawedge.consts
 
 class MyMethods {
-    companion object {
-        const val sendDataWedgeCommandStringParameter = "sendDataWedgeCommandStringParameter"
-        const val createDataWedgeProfile = "createDataWedgeProfile"
-        const val updateDataWedgeProfile = "updateDataWedgeProfile"
-        const val listenScannerStatus = "listenScannerStatus"
-    }
+  companion object {
+    const val getPlatformVersion ="getPlatformVersion"
+    const val sendDataWedgeCommandStringParameter = "sendDataWedgeCommandStringParameter"
+    const val createDataWedgeProfile = "createDataWedgeProfile"
+    const val listenScannerStatus = "listenScannerStatus"
+  }
 }
